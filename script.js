c8d1ba7a96689ab07b1738f4426d0d2466818949
Riley Hoff <rileyhoff@outlook.com>
2024-02-13 09:49:40 -0500

    add new slider for scope sample images

--- script.js
+++ script.js
@@ -40,6 +40,33 @@ function mblView(el) {
     document.getElementById("frame_wrap").scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 
 }
+
+//scrolls slider to slide of inputed number
+function skipToSlide(slider, num) {
+    var image = slider.children[num];
+    image.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
+}
+
+//returns the current slide number depending on current scroll position
+function getCurrentSlide(slider,total) {
+    var x = slider.scrollLeft;
+    var slideW = slider.scrollWidth / total;
+    slideW = (slider.scrollWidth - slideW) / total;
+    return(Math.floor(x / slideW));
+}
+
+function changeGrid(el) {
+    if (el.classList.contains("sm") == false) {
+        document.getElementById("sample_works").classList.add("sm");
+        el.classList.add("sm");
+        el.innerHTML = "large grid";
+    }else {
+        document.getElementById("sample_works").classList.remove("sm");
+        el.classList.remove("sm");
+        el.innerHTML = "thumbnails";
+    }
+}
+
 function expandFrame(el) {
     document.getElementById("frame_wrap").classList.toggle("expanded");
     el.classList.toggle("expanded");
@@ -47,26 +74,10 @@ function expandFrame(el) {
         el.innerHTML = "Shrink &#x2196;";
     }
     else { el.innerHTML = "Fullscreen &#x2197;"; }
-    document.getElementById("frame_wrap").scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    document.getElementById("frame_wrap").scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
 
 }
 function about() {
     document.getElementById("about").classList.toggle("open");
     document.getElementById("home").classList.toggle("left");
-}
-
-function imgView(el) {
-    //scroll to correct postion ( below scroll to view element )
-    var scrollPos = document.getElementById("scroll_to_view");
-    scrollPos.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    //change image and title
-    var fullView = document.getElementById("full_view");
-    var imgSrc = el.firstChild.src;
-    document.getElementById("full_img").src = imgSrc;
-    document.getElementById("full_title").innerHTML = el.firstChild.nextSibling.innerHTML;
-
-    fullView.classList.add("open");
-}
-function imgViewClose() {
-    document.getElementById("full_view").classList.remove("open");
 }
